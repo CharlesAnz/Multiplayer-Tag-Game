84f927e55f92a4db49d2cfbaf29a352709f38ca5
Charles <46895814+CharlesAnz@users.noreply.github.com>
2021-03-21 19:55:40 -0500

    Added Chat functions to SampleScene

---
+++ Photon Unity Networking Test/Assets/CharlesFolder/ChatManager.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using Photon.Chat;
+using ExitGames.Client.Photon;
+using AuthenticationValues = Photon.Chat.AuthenticationValues;
+
+public class ChatManager : MonoBehaviour, IChatClientListener
+{
+    [SerializeField]
+    private Text nickname, status, room, players, chat, StateText;
+
+    public InputField InputFieldChat;
+    public string UserName;
+
+    protected internal ChatAppSettings chatAppSettings;
+    ChatNetworkManager chatNetworkManager;
+    ChatClient chatClient;
+
+    public void Start()
+    {
+        chatNetworkManager = FindObjectOfType<ChatNetworkManager>();
+        this.chatAppSettings = PhotonNetwork.PhotonServerSettings.AppSettings.GetChatSettings();
+        // In the C# SDKs, the callbacks are defined in the `IChatClientListener` interface.
+        // In the demos, we instantiate and use the ChatClient class to implement the IChatClientListener interface.
+        chatClient = new ChatClient(this);
+        // Set your favourite region. "EU", "US", and "ASIA" are currently supported.
+        chatClient.ChatRegion = "EU";
+    }
+
+    public void Update()
+    {
+        chatClient.Service();
+    }
+
+    public void OnEnterSend()
+	{
+		if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter))
+		{
+		    this.SendChatMessage(this.InputFieldChat.text);
+			this.InputFieldChat.text = "";
+		}
+	}
+
+    public void DisableInput()
+    {
+
+    }
+
+    private void SendChatMessage(string inputLine)
+    {
+        chatClient.PublishMessage("Public", inputLine);
+    }
+    public void Connect()
+    {
+        this.chatClient = new ChatClient(this);
+        this.chatClient.AuthValues = new AuthenticationValues(this.UserName);
+        this.chatClient.UseBackgroundWorkerForSending = true;
+        this.chatClient.ConnectUsingSettings(this.chatAppSettings);
+
+        Debug.Log("Connecting as: " + this.UserName);
+    }
+
+    public void ShowChannel(string channelName)
+    {
+        if (string.IsNullOrEmpty(channelName))
+        {
+            return;
+        }
+
+        ChatChannel channel = null;
+        bool found = this.chatClient.TryGetChannel(channelName, out channel);
+        if (!found)
+        {
+            Debug.Log("ShowChannel failed to find channel: " + channelName);
+            return;
+        }
+
+        this.chat.text = channel.ToStringMessages();
+        Debug.Log("ShowChannel: Public");
+
+    }
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        if (level == DebugLevel.ERROR)
+        {
+            Debug.LogError(message);
+        }
+        else if (level == DebugLevel.WARNING)
+        {
+            Debug.LogWarning(message);
+        }
+        else
+        {
+            Debug.Log(message);
+        }
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+        this.StateText.text = state.ToString();
+    }
+
+    public void OnConnected()
+    {
+        chatClient.Subscribe("Public", 0);
+        InputFieldChat.gameObject.SetActive(true);
+    }
+
+    public void OnDisconnected()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        if (channelName.Equals("Public"))
+        {
+            // update text
+            this.ShowChannel("Public");
+        }
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        Console.WriteLine("Status change for: {0} to: {1}", user, status);
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        Debug.Log("OnSubscribed: " + string.Join(", ", channels));
+        this.ShowChannel(channels[0]);
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Photon Unity Networking Test/Assets/CharlesFolder/ChatNetworkManager.cs
+++ Photon Unity Networking Test/Assets/CharlesFolder/ChatNetworkManager.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
+using Photon.Chat;
+using ExitGames.Client.Photon;
 
-
-public class NetworkManager : MonoBehaviourPunCallbacks
+public class ChatNetworkManager : MonoBehaviourPunCallbacks 
 {
     [SerializeField]
     private Text nickname, status, room, players;
     [SerializeField]
     private Button buttonPlay, buttonLeave;
     [SerializeField]
-    public InputField playerName;
+    private InputField playerName;
     [SerializeField]
     private byte maxPlayersPerRoom = 4;
 
     public GameObject player;
+
+    ChatManager chatManager;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         buttonPlay.gameObject.SetActive(false);
         buttonLeave.gameObject.SetActive(false);
         playerName.gameObject.SetActive(false);
+
+        chatManager = FindObjectOfType<ChatManager>();
 
         if (!PhotonNetwork.IsConnected)
             PhotonNetwork.ConnectUsingSettings();
@@ -69,6 +74,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         buttonPlay.gameObject.SetActive(false);
         playerName.gameObject.SetActive(false);
         buttonLeave.gameObject.SetActive(true);
+        chatManager.UserName = PhotonNetwork.NickName;
+        chatManager.Connect();
         PhotonNetwork.Instantiate(player.name, new Vector3(Random.Range(-15, 15), 1, Random.Range(-15, 15)), Quaternion.Euler(0, Random.Range(-180, 180), 0), 0);
     }
 
@@ -113,5 +120,55 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         }
         else
             nickname.text = room.text = players.text = "";
+    }
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDisconnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        throw new System.NotImplementedException();
     }
 }
