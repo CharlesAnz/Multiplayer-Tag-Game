75fca55bfe7e0951f14dafd4b78e561351ecaf27
Stephen <k1446692@kingston.ac.uk>
2021-03-26 19:34:19 +0000

    Update NewPlayerScript.cs

--- Photon Unity Networking Test/Assets/Mike'sFolder/NewPlayerScript.cs
+++ Photon Unity Networking Test/Assets/Mike'sFolder/NewPlayerScript.cs
@@ -175,7 +175,7 @@ public class NewPlayerScript : MonoBehaviour
     {
         CanGiveBomb = false;
         HasBomb = true;
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(3.0f);
         CanGiveBomb = true;
         print("Should be able to pass bomb now");
     }
